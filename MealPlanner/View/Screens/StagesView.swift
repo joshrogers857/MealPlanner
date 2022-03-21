bc50bcd189c0924b6ee6d724bb9199d922e24f8f
joshrogers857 <87278992+joshrogers857@users.noreply.github.com>
2022-03-21 16:49:15 +0000

    Add stages page

---
+++ MealPlanner/View/Screens/StagesView.swift
@@ -0,0 +1,24 @@
+//
+//  StagesView.swift
+//  MealPlanner
+//
+//  Created by Joshua Rogers on 21/03/2022.
+//
+
+import SwiftUI
+
+struct StagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    init(mealPlan: MealPlan) {
+        
+    }
+}
+
+struct StagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        StagesView(mealPlan: MealPlan())
+    }
+}
