133480248faf8e07769beb895726c30ddc42e3fd
joshrogers857 <87278992+joshrogers857@users.noreply.github.com>
2022-03-31 12:53:35 +0100

    Implement first fields

--- MealPlanner/View/Screens/EditRecipeView.swift
+++ MealPlanner/View/Screens/EditRecipeView.swift
@@ -8,13 +8,103 @@
 import SwiftUI
 
 struct EditRecipeView: View {
+    
+    @FocusState private var keyboardIsFocused: Bool
+    @Environment(\.isPresented) var isPresented
+    
+    private var recipe: Recipe
+    
+    @State private var name: String
+    @State private var preparationTime: Double
+    @State private var cookingTime: Double
+    @State private var serves: Int16
+    @State private var listType: String = "Ingredients"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Form {
+                Section("Name") {
+                    TextField("Name", text: $name)
+                        .focused($keyboardIsFocused)
+                }
+                
+                Section("Prep. time") {
+                    TextField("Prep. time",
+                              value: $preparationTime,
+                              format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($keyboardIsFocused)
+                }
+                
+                Section("Cooking time") {
+                    TextField("Cooking time",
+                              value: $cookingTime,
+                              format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($keyboardIsFocused)
+                }
+                
+                Section("Serves") {
+                    TextField("Serves",
+                              value: $serves,
+                              format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($keyboardIsFocused)
+                }
+                
+                /* Section("Tag") {
+                    Picker("Tag", selection: $recipe.tag) {
+                        ForEach(RecipeTag.allCases, id: \.self) {
+                            value in
+                            
+                            Text(value.localizedName).tag(value)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Picker("List type", selection: $listType) {
+                    Text("Ingredients").tag("Ingredients")
+                    Text("Instructions").tag("Instructions")
+                }
+                .pickerStyle(.segmented)*/
+            }
+        .navigationTitle("Edit \(recipe.wrappedName)")
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+                
+                Button("Done") {
+                    keyboardIsFocused = false
+                }
+            }
+        }
+        .onChange(of: isPresented) {
+            newValue in
+            
+            if(newValue == false) {
+                recipe.name = name
+                recipe.preparationTime = preparationTime
+                recipe.cookingTime = cookingTime
+                recipe.serves = serves
+                
+                PersistenceController.shared.save()
+            }
+            
+        }
+    }
+    
+    init(recipe: Recipe) {
+        self.recipe = recipe
+        
+        name = recipe.name ?? ""
+        preparationTime = recipe.preparationTime
+        cookingTime = recipe.cookingTime
+        serves = recipe.serves
     }
 }
 
-struct EditRecipeView_Previews: PreviewProvider {
+/* struct EditRecipeView_Previews: PreviewProvider {
     static var previews: some View {
         EditRecipeView()
     }
-}
+} */
--- MealPlanner/View/Screens/RecipeSummaryView.swift
+++ MealPlanner/View/Screens/RecipeSummaryView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct RecipeSummaryView: View {
-    let recipe: Recipe
-    let scaledTo: Int?
-    var scalePercentage: Double? {
+    @FetchRequest private var recipe: FetchedResults<Recipe>
+    private let scaledTo: Int?
+    private var scalePercentage: Double? {
         if(scaledTo != nil) {
-            return Double(scaledTo!) / Double(recipe.serves) //Assert not nil because of if statement
+            return Double(scaledTo!) / Double(recipe[0].serves) //Assert not nil because of if statement
         } else {
             return nil
         }
@@ -20,35 +20,44 @@ struct RecipeSummaryView: View {
     
     var body: some View {
         VStack {
-            Text("\(recipe.wrappedTag) recipe")
+            Text("\(recipe[0].wrappedTag) recipe")
                 .font(.caption)
             
             Spacer()
             
             TimingsView(
-                preparationTime: recipe.preparationTime,
-                cookingTime: recipe.cookingTime
+                preparationTime: recipe[0].preparationTime,
+                cookingTime: recipe[0].cookingTime
             )
             
-            ImageView(name: recipe.wrappedName)
+            ImageView(name: recipe[0].wrappedName)
             
-            ServesView(serves: Int(recipe.serves), scaledTo: scaledTo)
+            ServesView(serves: Int(recipe[0].serves), scaledTo: scaledTo)
             
             IngredientInstructionListView(
-                ingredients: recipe.recipeIngredientsArray,
-                instructions: recipe.instructionsArray,
+                ingredients: recipe[0].recipeIngredientsArray,
+                instructions: recipe[0].instructionsArray,
                 scaleToPercentage: scalePercentage
             )
         }
-        .navigationTitle(recipe.wrappedName)
+        .navigationTitle(recipe[0].wrappedName)
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItem(placement: .primaryAction) {
-                NavigationLink(destination: EditRecipeView()) {
+                NavigationLink(destination: EditRecipeView(recipe: recipe[0])) {
                     Text("Edit")
                 }
             }
         }
+    }
+    
+    init(recipe: Recipe, scaledTo: Int?) {
+        _recipe = FetchRequest<Recipe>(
+            sortDescriptors: [],
+            predicate: NSPredicate(format: "self == %@", recipe)
+        )
+        
+        self.scaledTo = scaledTo
     }
 }
 
