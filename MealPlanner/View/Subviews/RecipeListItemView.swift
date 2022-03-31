9dba028cb028c84559350fa3f01625b3f5f57441
joshrogers857 <87278992+joshrogers857@users.noreply.github.com>
2022-03-31 14:26:39 +0100

    Fix RecipeListItemView reactivity

--- MealPlanner/View/Subviews/RecipeListItemView.swift
+++ MealPlanner/View/Subviews/RecipeListItemView.swift
@@ -8,26 +8,35 @@
 import SwiftUI
 
 struct RecipeListItemView: View {
-    let recipe: Recipe
+    @FetchRequest private var recipe: FetchedResults<Recipe>
     let scaledTo: Int?
     
     var body: some View {
-        NavigationLink(destination: RecipeSummaryView(recipe: recipe, scaledTo: scaledTo)) {
+        NavigationLink(destination: RecipeSummaryView(recipe: recipe[0], scaledTo: scaledTo)) {
             HStack {
                 ZStack {
                     Color.black
                         .frame(width: 102, height: 62, alignment: .leading)
                 
-                    Image(decorative: recipe.wrappedName)
+                    Image(decorative: recipe[0].wrappedName)
                         .resizable()
                         .frame(width: 100, height: 60, alignment: .leading)
                 }
                 
-                Text(recipe.wrappedName)
-                    .tag(recipe.wrappedName)
+                Text(recipe[0].wrappedName)
+                    .tag(recipe[0].wrappedName)
                     .padding()
             }
         }
+    }
+    
+    init(recipe: Recipe, scaledTo: Int?) {
+        _recipe = FetchRequest<Recipe>(
+            sortDescriptors: [],
+            predicate: NSPredicate(format: "self == %@", recipe)
+        )
+        
+        self.scaledTo = scaledTo
     }
 }
 
